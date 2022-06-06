309339bccc0d8bd34e6b0add52cfccf8cf13985a
jdrc2118 <jdrc2118@gmail.com>
2022-06-06 15:41:02 -0500

    cambios

--- app/routes.js
+++ app/routes.js
@@ -1,3 +1,8 @@
+
+var path = require('path');
+var routes = function(app) {
 app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname + '/public', 'index.html'));
 });
+};
+module.exports = routes;
